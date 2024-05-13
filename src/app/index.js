1c3e01f90eb7017cd52f54ecd692503f4b68d459
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-05-13 10:37:48 +0300

    added bd

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import router from '../routs/index.js';
+import mongoose from 'mongoose';
+mongoose.connect('mongodb+srv://dimatuzkoff:5uPMUnhRxmzsA3cx@cluster0.ogdrcr6.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0');
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
 
 const app = express();
 
