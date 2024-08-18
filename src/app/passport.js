4c39a1a599e70adc774e2c2c95e9b65b15c9e4be
DariaZhur <dariazhur89@gmail.com>
2024-08-18 23:22:17 +0300

    fix passport

--- src/app/passport.js
+++ src/app/passport.js
@@ -2,13 +2,13 @@ import passport from 'passport';
 import User from '../modeles/user.js';
 import { Strategy as LocalStrategy } from 'passport-local';
 import crypto from 'crypto';
+
 function passportInit(app) {
     app.use(passport.initialize());
     app.use(passport.session());
 
-
     passport.use('local-login', new LocalStrategy((username, password, done) => {
-        User.findOne({ username: username }).exec() // Уберите обратный вызов здесь
+        User.findOne({ username: username }).exec()
             .then(user => {
                 if (!user) {
                     return done(null, false, { message: 'Incorrect username or password.' });
@@ -24,40 +24,18 @@ function passportInit(app) {
             .catch(err => done(err));
     }));
 
-
-
-
-
-
-
-
-
-    // passport.use(new LocalStrategy((username, password, done) => {
-    //     User.findOne({ username: username }, (err, user) => {
-    //         if (err) { return done(err); }
-    //         if (!user) { return done(null, false, { message: 'Incorrect username or password.' }); }
-
-    //         crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', (err, hashedPassword) => {
-    //             if (err) { return done(err); }
-    //             if (!crypto.timingSafeEqual(Buffer.from(user.hashed_password, 'hex'), hashedPassword)) {
-    //                 return done(null, false, { message: 'Incorrect username or password.' });
-    //             }
-    //             return done(null, user);
-    //         });
-    //     });
-    // }));
-
-    passport.serializeUser(function (user, done) {
+    passport.serializeUser((user, done) => {
         done(null, user.id);
     });
 
-    passport.deserializeUser(function (id, done) {
-        // Найдите пользователя в вашей базе данных по id
-        User.findById(id, function (err, user) {
-            done(err, user);
-        });
+    passport.deserializeUser(async (id, done) => {
+        try {
+            const user = await User.findById(id).exec(); // Изменено на использование exec()
+            done(null, user);
+        } catch (err) {
+            done(err);
+        }
     });
-
 }
 
 export default passportInit;
