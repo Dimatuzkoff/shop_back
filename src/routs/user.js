afe8b8390d1f25a4eff06de0b4398d11aae17fd6
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-10-10 17:56:04 +0300

    fix

--- src/routs/user.js
+++ src/routs/user.js
@@ -1,12 +1,14 @@
 import express from "express";
+import User from "../modeles/user.js";
 
 
 const router = express.Router();
 
-router.put("/user", (req, res) => {
+router.put("/user", async (req, res) => {
     const userId = req.user._id;
     console.log(req.user);
-    res.json({ ok: true, message: "User saved successfully", user: req.user });
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+    res.json({ ok: true, message: "User saved successfully", user: updatedUser });
 });
 
 
