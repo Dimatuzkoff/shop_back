5f0af1004f62ab9d52f7bda59b817c4583fe91de
Dimatuzkoff <dimatuzkoff@gmail.com>
2024-10-31 11:09:01 +0200

    added socket server to index

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 import http from 'http';
 import app from './src/app/index.js';
 import 'dotenv/config'
-import io from './socket-server.js'
+import { Server } from 'socket.io';
 
 const server = http.createServer(app);
 console.log("Сервер работает!");
+// Создаем Socket.IO сервер
+
+const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:3000", "https://shop-front-nine.vercel.app"], // Укажите адрес вашего фронтенда
+        methods: ['GET', 'POST'],
+    },
+});
 
 server.listen(3001)
+
+
+
+const clients = [];
+
+io.on('connection', (socket) => {
+    console.log('Пользователь подключен:', socket.id);
+    // console.log('список; ', io.sockets.sockets);
+    clients.push(socket);
+    console.log('Клиентов подключено:', clients.length);
+
+
+
+    // Обработка входящих сообщений от клиента
+    socket.on('message', (msg) => {
+        console.log('Сообщение от клиента:', msg);
+        io.emit('message', msg); // Рассылка сообщения всем клиентам
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Пользователь отключен:', socket.id);
+        clients.splice(clients.indexOf(socket), 1);
+    });
+});
